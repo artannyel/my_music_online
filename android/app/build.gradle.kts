417f0bfb09b9db91bd272e73925c9773fb5f0b1c
Francisco Artannyel Pinto Ferreira <francisco.ferreira@icts.com.br>
2026-07-19 16:41:59 -0300

    chore(android): add Google Services plugin and Firebase BOM dependency

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -2,6 +2,12 @@ plugins {
     id("com.android.application")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+
+    id("com.google.gms.google-services")
+}
+
+dependencies {
+  implementation(platform("com.google.firebase:firebase-bom:34.16.0"))
 }
 
 android {
